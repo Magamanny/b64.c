806b3a6f055cec81798297e24b565bcde357d8c7
Ali Harman <ali.harman.ansari@outlook.com>
2021-10-07 13:40:24 +0500

    removed buffer.c
    * Added comments on functions.
    * Added memory management on the user side.

--- test.c
+++ test.c
@@ -11,38 +11,26 @@
 #include <assert.h>
 #include "b64.h"
 
-#define S(x) # x
-#define t(m, a, b) ({                                                \
-    char tmp[1024];                                                  \
-    sprintf(tmp, "%s(%s) = %s", S(m), S(a), S(b));                   \
-    char *r = (char *) m(a, strlen((char *) a));                     \
-    assert(0 == strcmp(b, r));                                       \
-    free(r);                                                         \
-})
+#define S(x) #x
+#define t(m, a, b) (                                 \
+    {                                                \
+      char tmp[1024];                                \
+      char r[1024];                                  \
+      sprintf(tmp, "%s(%s) = %s", S(m), S(a), S(b)); \
+      m(a, r, strlen((char *)a));                    \
+      assert(0 == strcmp(b, r));                     \
+    })
 
-void* custom_malloc(size_t size){
-  if (size == 0){
-    /* On some systems malloc doesn't allow for size = 0 */
-    return NULL;
-  }
-  return malloc(size);
-}
-
-void* custom_realloc(void* ptr, size_t size){
-  return realloc(ptr, size);
-}
-
-int
-main (void) {
-
+int main(void)
+{
   // encode
   {
-    t(b64_encode, (const unsigned char *) "bradley", "YnJhZGxleQ==");
-    t(b64_encode, (const unsigned char *) "kinkajou", "a2lua2Fqb3U=");
-    t(b64_encode, (const unsigned char *) "vino", "dmlubw==");
+    t(b64_encode, (const unsigned char *)"bradley", "YnJhZGxleQ==");
+    t(b64_encode, (const unsigned char *)"kinkajou", "a2lua2Fqb3U=");
+    t(b64_encode, (const unsigned char *)"vino", "dmlubw==");
     t(b64_encode,
-        (const unsigned char *) "brian the monkey and bradley the kinkajou are friends",
-        "YnJpYW4gdGhlIG1vbmtleSBhbmQgYnJhZGxleSB0aGUga2lua2Fqb3UgYXJlIGZyaWVuZHM=");
+      (const unsigned char *)"brian the monkey and bradley the kinkajou are friends",
+      "YnJpYW4gdGhlIG1vbmtleSBhbmQgYnJhZGxleSB0aGUga2lua2Fqb3UgYXJlIGZyaWVuZHM=");
   }
 
   // decode
@@ -51,8 +39,8 @@ main (void) {
     t(b64_decode, "aGF4", "hax");
     t(b64_decode, "bW9ua2V5cyBhbmQgZG9ncw==", "monkeys and dogs");
     t(b64_decode,
-        "dGhlIGtpbmtham91IGFuZCBtb25rZXkgZm91Z2h0IG92ZXIgdGhlIGJhbmFuYQ==",
-        "the kinkajou and monkey fought over the banana");
+      "dGhlIGtpbmtham91IGFuZCBtb25rZXkgZm91Z2h0IG92ZXIgdGhlIGJhbmFuYQ==",
+      "the kinkajou and monkey fought over the banana");
   }
 
   printf("Ok done");
